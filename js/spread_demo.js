7e1123e3107b3798088e020fa768c329ad019ef9
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-03-11 11:21:18 +0100

    added files

--- js/spread_demo.js
+++ js/spread_demo.js
@@ -8,5 +8,29 @@ let  cohort ={
 
 let  newCohort=cohort
 newCohort.name="interesting stuff"
-console.log(newCohort.name)
+// console.log(newCohort.name)
+// console.log(cohort.name)
+
+//spread
+let  cohortNew={...cohort}
+
+cohortNew.name="cool stuff"
+
+
+console.log(cohortNew.name)
 console.log(cohort.name)
+console.log(newCohort.name)
+
+console.log()
+//rest
+function  doSomething(...parameter){
+  // console.log(parameter)
+
+
+
+  (function (){
+    parameter.forEach((parameter) => console.log("hi " + parameter))
+  })()
+}
+
+doSomething("ujay", "increase", "mj", "remi")
